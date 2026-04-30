c1636b6755ad557d420d73190b6780ee67b296bf
fegasi <felgasi@gmail.com>
2026-04-30 13:04:29 -0600

    db prueba

--- app/src/main/java/com/example/myapp10test/restApi/ConstantesRestApi.java
+++ app/src/main/java/com/example/myapp10test/restApi/ConstantesRestApi.java
@@ -4,13 +4,14 @@ package com.example.myapp10test.restApi;
 public final class ConstantesRestApi {
 
     //https://api.instagram.com/v1/
-    public static final String VERSION = "/v1/";
-    public static final String ROOT_URL = "https://api.instagram.com" + VERSION;
-    public static final String ACCESS_TOKEN = "3259702353.aa0d0f4.e61865afc9144ecc8ffc8f6dc84aa17b";
+    public static final String VERSION = "/v25.0/";
+    public static final String ROOT_URL = "https://graph.instagram.com" + VERSION;
+    public static final String ACCESS_TOKEN = "IGAAhxTr7NiJpBZAFlLaXVnRDZAkc1lOSlJFVDE0dzBLUTVKQXhrcDBvQmg5d0V0M2JtLUJLSjd0S1Q4N28ydUUzZA0hObllIU1l0X1JHb016Tk5PWmhPaEcydVNPUzhnM2JIcW9KOE5oNFpFY2c3bk9OVklmT2xvTFJVOGVtU00zUQZDZD";
     public static final String KEY_ACCESS_TOKEN = "?access_token=";
     public static final String KEY_GET_RECENT_MEDIA_USER = "users/self/media/recent/";
     public static final String URL_GET_RECENT_MEDIA_USER = KEY_GET_RECENT_MEDIA_USER + KEY_ACCESS_TOKEN + ACCESS_TOKEN;
     //https://api.instagram.com/v1/users/self/media/recent/?access_token=ACCESS-TOKEN
+    //https://graph.instagram.com/v25.0/me/media/recent?access_token=IGAAhxTr7NiJpBZAFlLaXVnRDZAkc1lOSlJFVDE0dzBLUTVKQXhrcDBvQmg5d0V0M2JtLUJLSjd0S1Q4N28ydUUzZA0hObllIU1l0X1JHb016Tk5PWmhPaEcydVNPUzhnM2JIcW9KOE5oNFpFY2c3bk9OVklmT2xvTFJVOGVtU00zUQZDZD
 
 
 
